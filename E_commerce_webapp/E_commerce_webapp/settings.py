8642ea92d770e2b2a18bd584cba7f5434bdc8bfe
Wronghand-hash <62405360+Wronghand-hash@users.noreply.github.com>
2020-10-22 14:13:10 +0330

    3 video completed
    created a footer and move 'contanct link down'
    created an about page
    made possible to set ordering for the categories
    added ordering to products on the front page
    created folder and app for the cart
    created a view for the cart and show link in the menu

--- E_commerce_webapp/E_commerce_webapp/settings.py
+++ E_commerce_webapp/E_commerce_webapp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.core',
     'apps.store',
+    'apps.cart',
     'accounts',
 ]
 
--- E_commerce_webapp/apps/store/models.py
+++ E_commerce_webapp/apps/store/models.py
@@ -4,9 +4,11 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
+    ordering = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'categories'
+        ordering = ('ordering',)
 
 
     def __str__(self):
@@ -19,6 +21,11 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
+    is_featured = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_added',)
 
     def __str__(self):
         return self.title
